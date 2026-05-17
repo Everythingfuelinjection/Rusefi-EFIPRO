25eff45981b20c0a63f6b6623e887a3280800fb2
GitHub git update Action <action@github.com>
2026-05-17 02:51:57 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_rusEFI_EFIPRO.h
+++ generated/controllers/generated/rusefi_generated_rusEFI_EFIPRO.h
@@ -1569,7 +1569,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3209675757
+#define SIGNATURE_HASH 1018192380
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2184,7 +2184,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.05.16.rusEFI_EFIPRO.3209675757"
+#define TS_SIGNATURE "rusEFI main.2026.05.17.rusEFI_EFIPRO.1018192380"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_rusEFI_EFIPRO.h
+++ generated/controllers/generated/signature_rusEFI_EFIPRO.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3209675757
-#define TS_SIGNATURE "rusEFI main.2026.05.16.rusEFI_EFIPRO.3209675757"
+#define SIGNATURE_HASH 1018192380
+#define TS_SIGNATURE "rusEFI main.2026.05.17.rusEFI_EFIPRO.1018192380"
